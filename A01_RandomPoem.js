38ec0bc26da45af5052947082692b42dc34fe3be
Max Wronka <wronka.ma.phi@gmail.com>
2021-03-26 16:12:09 +0100

    init

--- A01_RandomPoem.js
+++ A01_RandomPoem.js
@@ -1,16 +1,23 @@
 "use strict";
 var A01_RandomPoem;
 (function (A01_RandomPoem) {
-    let n = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let v = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let o = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    let;
+    let subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verb = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(subject, verb, object);
     //Schleife:
-    for (let i = 0; i < n.length; i++) {
-        //if (a == 0){
-        //break;
-        //}
-        console.log(i);
+    for (let i = subject.length; i > 0; i--) {
+        // console.log(i);
+        console.log(getVerse(subject, verb, object));
+    }
+    //console.log(subjekt, verb, objekt, satz);
+    function getVerse(_subject, _verb, _object) {
+        let verse = "";
+        let sub = Math.floor(Math.random() * _subject.length);
+        let ver = Math.floor(Math.random() * _verb.length);
+        let obj = Math.floor(Math.random() * _object.length);
+        verse = _subject.splice(sub, 1)[0] + " " + _verb.splice(ver, 1)[0] + " " + _object.splice(obj, 1)[0] + ".";
+        return verse;
     }
 })(A01_RandomPoem || (A01_RandomPoem = {}));
 //# sourceMappingURL=A01_RandomPoem.js.map
--- A01_RandomPoem.ts
+++ A01_RandomPoem.ts
@@ -2,16 +2,22 @@ namespace A01_RandomPoem {
     let subject: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verb: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let object: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    let sentence: string = "";
     
     console.log(subject, verb, object);
 
     //Schleife:
-    for (var i: number = 6; i <= subject.length; i--) {
-        //if (a == 0){
-            //break;
-        //}
-        console.log(i);
+    for (let i: number = subject.length; i > 0; i--) {
+        // console.log(i);
+        console.log(getVerse(subject, verb, object));
     }
     //console.log(subjekt, verb, objekt, satz);
+
+    function getVerse (_subject:string[], _verb:string[], _object:string[]): string {
+        let verse: string = "";
+        let sub: number = Math.floor(Math.random() * _subject.length);
+        let ver: number = Math.floor(Math.random() * _verb.length);
+        let obj: number = Math.floor(Math.random() * _object.length);
+        verse = _subject.splice(sub,1)[0] + " " + _verb.splice(ver,1)[0] + " " + _object.splice(obj,1)[0] + ".";
+        return verse;
+    }
 }
