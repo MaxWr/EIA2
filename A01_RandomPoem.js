3c2b461b531dc14ebf9ea009a6af82c8576e7fbb
Max Wronka <wronka.ma.phi@gmail.com>
2021-03-26 14:58:32 +0100

    init

---
+++ A01_RandomPoem.js
@@ -0,0 +1,16 @@
+"use strict";
+var A01_RandomPoem;
+(function (A01_RandomPoem) {
+    let n = ["Daenerys Targaryen ", "Jon Snow ", "Arya Stark ", "Sansa Stark ", "Eddard Stark ", "Joffrey Baratheon "];
+    let v = ["kauft ", "benutzt ", "vergisst ", "sucht ", "liebt ", "hasst "];
+    let o = ["Schwerter ", "Tröne ", "Familie Baratheon ", "Kronen ", "Kriege ", "Drachen "];
+    let;
+    //Schleife:
+    for (let i = 0; i < n.length; i++) {
+        //if (a == 0){
+        //break;
+        //}
+        console.log(i);
+    }
+})(A01_RandomPoem || (A01_RandomPoem = {}));
+//# sourceMappingURL=A01_RandomPoem.js.map
--- A01_RandomPoem.ts
+++ A01_RandomPoem.ts
@@ -1,2 +1,17 @@
-let v = "Expecto Patronum";
-console.log(v);
+namespace A01_RandomPoem {
+    let subjekt: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verb: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekt: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let satz: string = "";
+    
+    //console.log(subjekt, verb, objekt);
+
+    //Schleife:
+    for (var i: number = 6; i <= subjekt.length; i--) {
+        //if (a == 0){
+            //break;
+        //}
+        console.log(i);
+    }
+    //console.log(subjekt, verb, objekt, satz);
+}
